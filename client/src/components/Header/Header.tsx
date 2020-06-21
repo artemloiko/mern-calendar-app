1de8bc48db9ba4f41721b64782afae7e49008ce8
Artem Loiko <artuom130@gmail.com>
2020-06-21 18:38:00 +0300

    Update header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,5 +1,4 @@
 import React, { ReactElement } from 'react';
-import { Link } from '@reach/router';
 
 import './Header.css';
 
@@ -13,11 +12,7 @@ const Header: React.FC<HeaderProps> = (props) => {
   return (
     <header className="header">
       <div className="container header__container">
-        <h1 className="header__heading">
-          <Link to="/" className="header__heading-link">
-            MERN Calendar app
-          </Link>
-        </h1>
+        <h1 className="header__heading">MERN Calendar app</h1>
         <div className="header__control-group">{controls}</div>
       </div>
     </header>
